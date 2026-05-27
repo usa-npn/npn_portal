73e48de6804f97d4bdec0eb4cc9f34e5ad045e8e
Jeff Switzer <jeff.switzer@gmail.com>
2026-05-27 09:25:36 -0700

    remove ram migrations

--- js_migration_2/src/utils/checkProperty.js
+++
@@ -1,3 +0,0 @@
-module.exports = function checkProperty(obj, prop) {
-  return obj && Object.prototype.hasOwnProperty.call(obj, prop) && obj[prop] !== undefined && obj[prop] !== null;
-};
--- js_migration_2/src/utils/validateUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verifyUser: (accessToken, consumerKey) => {
-    // Mock logic — replace with real DB validation
-    if (accessToken === 'valid_token' && consumerKey === 'valid_key') {
-      return 123;
-    }
-    return null;
-  }
-};
